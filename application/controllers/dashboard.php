ae0b2e44b80ba3e188bf0816b9f61235454e4da7
Прищепа Сергей <rjhdsy@gmail.com>
2012-03-02 14:21:45 +0200

    Add twitter bootstrap

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
+
+/* End of file dashboard.php */
+/* Location: ./application/controllers/dashboard.php */
